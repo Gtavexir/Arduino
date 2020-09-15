f54b19f321322590e4aa47019176e2a6b7c60af4
Gtavexir <gtavexir@kookmin.ac.kr>
2020-09-15 09:00:40 +0900

    Add 05_partice

---
+++ 05_pratice/05_pratice_2/05_pratice_2.ino
@@ -0,0 +1,26 @@
+#define LED 7
+bool toggle = false;
+int cnt = 0;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, toggle);
+  delay(1000);
+}
+
+void loop() {
+
+  toggle = toggle_state(toggle);
+  digitalWrite(LED, toggle);
+  delay(100);
+  if(!toggle) cnt++;
+  
+  while(1) {
+    if(cnt < 5) break;
+    digitalWrite(LED, 1); //무한 루프로 종료
+  }
+}
+
+bool toggle_state(bool toggle) {
+  return !toggle;
+}
