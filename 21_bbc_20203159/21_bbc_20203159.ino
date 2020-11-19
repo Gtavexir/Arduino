d5898cf8e2dba9bc55b61ef939c07155308c5acc
Gtavexir <gtavexir@kookmin.ac.kr>
2020-11-19 22:32:07 +0900

    Bang Bang Control

---
+++ 21_bbc_20203159/21_bbc_20203159.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+
+// Arduino pin assignment
+#define PIN_SERVO 10
+#define PIN_IR A0
+#define PIN_LED 9
+Servo myservo;
+int a, b; // unit: mm
+void setup() {
+  myservo.attach(PIN_SERVO);
+  myservo.writeMicroseconds(1470);
+// initialize GPIO pins
+  pinMode(PIN_LED,OUTPUT);
+  digitalWrite(PIN_LED, 1);
+// initialize serial port
+  Serial.begin(57600);
+  a = 73;
+  b = 387;
+}
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+void loop() {
+  float raw_dist = ir_distance();
+  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+  Serial.print("min:0,max:500,dist:");
+  Serial.print(raw_dist);
+  Serial.print(",dist_cali:");
+  Serial.println(dist_cali);
+  if(dist_cali < 235.5) myservo.writeMicroseconds(1670);
+  else myservo.writeMicroseconds(1270);
+
+  
+  if(raw_dist > 156 && raw_dist <224) digitalWrite(PIN_LED, 0);
+  else digitalWrite(PIN_LED, 255);
+  delay(20);
+}
